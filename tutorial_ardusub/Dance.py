fd24bffe85983218b3f84f541f82c9eb0c8b078a
M1saka110032 <eugenewang920@gmail.com>
2025-07-16 21:16:03 -0400

    Add Light control

--- tutorial_ardusub/Dance.py
+++ tutorial_ardusub/Dance.py
@@ -2,7 +2,7 @@
 import rclpy
 from rclpy.node import Node
 from std_srvs.srv import SetBool
-from mavros_msgs.msg import ManualControl
+from mavros_msgs.msg import OverrideRCIn, ManualControl
 import time
 
 class Dance(Node):
@@ -11,18 +11,19 @@ class Dance(Node):
         self.cli = self.create_client(SetBool, '/arming')
         while not self.cli.wait_for_service(timeout_sec=1.0):
             self.get_logger().info('waiting /arming ...')
+        
         self.pub = self.create_publisher(ManualControl, "/manual_control", 10)
+        self.command_pub = self.create_publisher(OverrideRCIn, "override_rc", 10)
         self.call_service(True, "Vehicle armed")
         self.dance()
         
     
     def publish_manual_control(self,x,y,z,r):
-          # 10 times after close the node
         msg = ManualControl()
-        msg.x = x
-        msg.y = y
-        msg.z = z
-        msg.r = 50.0
+        msg.x = float(x)
+        msg.y = float(y)
+        msg.z = float(z)
+        msg.r = float(r)
         msg.buttons = 0
         self.pub.publish(msg)
         self.get_logger().info("Moving: x={}, y={}, z={}".format(msg.x, msg.y, msg.z))
@@ -40,20 +41,34 @@ class Dance(Node):
             self.get_logger().error(f'Service call failed: {future.result()}')
         return future.result()
     
+    def turn_lights_on(self, level):
+        """
+        Turn the lights on.
+
+        Args:
+            level (int): The level to turn the lights on to. 0 is off, 100 is full
+        """
+        self.get_logger().info(f"Turning lights on to level {level}")
+        commands = OverrideRCIn()
+        commands.channels = [OverrideRCIn.CHAN_NOCHANGE] * 10
+        commands.channels[8] = 1000 + level * 10
+        commands.channels[9] = 1000 + level * 10
+        self.command_pub.publish(commands)
+    
     def dance(self):
         moves = [
-            (0, 0, 0, 0),
-            (50, 0, 0,-30),
-            (50, 0, 0,-30),
-            (50, 0, 0,-30),
-            (-50, 0, 0,30),
-            (-50, 0, 0,30),
-            (-50, 0, 0,30),
+            (0, 0, 0, 0, 0),
+            (50, 0, 0,-30,0),
+            (50, 0, 0,-30,0),
+            (50, 0, 0,-30,0),
+            (-50, 0, 0,30,0),
+            (-50, 0, 0,30,0),
+            (-50, 0, 0,30,0),
         ]
-        for x, y, z, r in moves:
-            x, y, z, r = float(x),float(y),float(z),float(r)
+        for x, y, z, r, level in moves:
             self.get_logger().info(f"x: {x} y: {y} z: {z} r: {r}")
             self.publish_manual_control(x ,y, z, r)
+            self.turn_lights_on(level)
             time.sleep(1.0)
         self.call_service(False, "Vehicle disarmed.")
 
